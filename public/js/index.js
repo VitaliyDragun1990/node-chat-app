5afcf5c2ca9f9fd5bf9387550f3b097a12d33c2c
Vitaly Dragun <vdrag00n90@gmail.com>
2018-01-26 15:31:27 +0200

    Add css for chat app

--- public/js/index.js
+++ public/js/index.js
@@ -33,30 +33,42 @@ socket.on('newLocationMessage', function (message) {
 
 });
 
+// listen submit event on button
 jQuery('#message-form').on('submit', function (e) {
     // prevent standard form submitting behavior
     e.preventDefault();
 
+    let messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val(''); // clean input field value
     });
 });
 
+// listen to 'Send Location' button click event
 let locationButton = jQuery('#send-location');
 locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.')
     }
 
+    // disabled button after send user current location to prevent spamming
+    locationButton.attr('disabled', 'disabled').text('Sending location ...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        // enable button after data was send
+        locationButton.removeAttr('disabled').text('Send location');
+        // emit new message containing user location data
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        // enable button even if  data wasn't send
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     })
 });
